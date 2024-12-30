d5f6ec4b698d62c22e8cc5e0f4b93d76c09f09f0
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-30 16:37:07 -0300

    feat: criação da tela de carros

---
+++ lib/screens/carros_screen.dart
@@ -0,0 +1,10 @@
+﻿import 'package:flutter/material.dart';
+
+class CarrosScreen extends StatelessWidget {
+  const CarrosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('todos os carrinhos', style: TextStyle(color: Colors.black),));
+  }
+}
