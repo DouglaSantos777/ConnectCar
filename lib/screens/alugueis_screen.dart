fe01c63f241462fdc98e20edfad78325fdb0fe7b
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-30 16:37:24 -0300

    feat: criação da tela de alugueis

---
+++ lib/screens/alugueis_screen.dart
@@ -0,0 +1,33 @@
+﻿import 'package:connectcar/screens/detalhes_aluguel_screen.dart';
+import 'package:connectcar/widgets/alugueis/card_aluguel.dart';
+import 'package:flutter/material.dart';
+
+class AlugueisScreen extends StatelessWidget {
+  const AlugueisScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Aluguéis'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              CardAluguel(onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) =>  const DetalhesAluguelScreen()
+                  )
+                );
+              },),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
---
+++ lib/widgets/alugueis/card_aluguel.dart
@@ -0,0 +1,35 @@
+﻿import 'package:flutter/material.dart';
+
+class CardAluguel extends StatelessWidget {
+  final VoidCallback onPressed;
+  const CardAluguel({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(
+        padding: EdgeInsets.zero,  
+      ),
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.only(bottom: 8),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 4, 86, 153),
+          borderRadius: BorderRadius.circular(8)
+        ),
+        child: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Aluguel de {categoria carro}', style: TextStyle(color: Colors.white, fontSize: 20),),
+              Text('Cliente {nome do cliente}', style: TextStyle(color: Colors.white70, fontSize: 16),),
+              Text('Data de retirada - Data de devolução', style: TextStyle(color: Colors.white70, fontSize: 16),),
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
