7dd9e9db00a5f2424fe2242fad1c729032ad78a6
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-30 16:38:57 -0300

    feat: criação de widgets botao e valor total

---
+++ lib/widgets/valor_total_estilo.dart
@@ -0,0 +1,28 @@
+﻿import 'package:flutter/material.dart';
+
+class ValorTotalEstilo extends StatelessWidget {
+  final String valorTotal;
+  const ValorTotalEstilo({super.key, required this.valorTotal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      padding: const EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          Text('Valor Total: R\$ $valorTotal', 
+            style: const TextStyle(
+              color: Colors.white, 
+              fontSize: 20,
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
