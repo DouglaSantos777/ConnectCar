6179656dd265a9cd45bc30ca54660e713cc6f820
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-30 16:38:08 -0300

    feat: criação da tela de historico

---
+++ lib/screens/historico_screen.dart
@@ -0,0 +1,10 @@
+﻿import 'package:flutter/material.dart';
+
+class HistoricoScreen extends StatelessWidget {
+  const HistoricoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('historicozinho', style: TextStyle(color: Colors.black),));
+  }
+}
