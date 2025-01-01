7516c123f3dcb32890e9b04347f9561a873137fb
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-31 21:09:27 -0300

    feat: criação da tela de detalhes do carro

---
+++ lib/screens/detalhes_carro_screen.dart
@@ -0,0 +1,19 @@
+﻿import 'package:connectcar/widgets/carros_widgets/card_informacoes_carro.dart';
+import 'package:flutter/material.dart';
+
+class DetalhesCarroScreen extends StatelessWidget {
+  const DetalhesCarroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalhes do carro'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: const CardCarroDetalhes(),
+      )
+    );
+  }
+}
