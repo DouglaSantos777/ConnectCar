209d9c5311975e6728a045a08af287a33ff75115
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-31 21:09:59 -0300

    feat: criação da tela que lista os carros

---
+++ lib/screens/lista_carros_screen.dart
@@ -0,0 +1,34 @@
+﻿import 'package:connectcar/widgets/carros_widgets/card_carro.dart';
+import 'package:flutter/material.dart';
+
+class ListaCarrosScreen extends StatelessWidget {
+  const ListaCarrosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Seus carros'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: const SingleChildScrollView(
+          child: Column(
+            children: [
+              CardCarro(),
+              CardCarro(),
+              CardCarro(),
+              CardCarro(),
+              CardCarro(),
+              CardCarro(),
+              CardCarro(),
+              CardCarro(),
+              CardCarro(),
+              CardCarro(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
