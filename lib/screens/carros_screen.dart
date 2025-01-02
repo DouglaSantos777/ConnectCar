b793ad0519c9cbb746bfd6376db40f15bcb82792
Maria Clara <mclaradearaujop@aluno.uespi.br>
2025-01-02 10:08:52 -0300

    feat: correção de estilo na tela geral de carros

--- lib/screens/carros_screen.dart
+++ lib/screens/carros_screen.dart
@@ -1,9 +1,10 @@
-﻿import 'package:connectcar/widgets/carros_widgets/secao_carros_alug.dart';
+﻿import 'package:connectcar/widgets/alugueis/orcamento.dart';
+import 'package:connectcar/widgets/carros_widgets/secao_carros_alug.dart';
 import 'package:connectcar/widgets/carros_widgets/secao_carros_disp.dart';
 import 'package:flutter/material.dart';
 
 class CarrosScreen extends StatelessWidget {
-  CarrosScreen({super.key});
+  const CarrosScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +14,13 @@ class CarrosScreen extends StatelessWidget {
       ),
       body: Container(
         padding: const EdgeInsets.all(16),
-        child: const Column(
-          children: [
-            SecaoCarrosDisponiveis(),
-            SecaoCarrosAlugados(),
-          ],
+        child: const SingleChildScrollView(
+          child: Column(
+            children: [
+              SecaoCarrosDisponiveis(),
+              SecaoCarrosAlugados(),
+            ],
+          ),
         ),
       ),
     );
